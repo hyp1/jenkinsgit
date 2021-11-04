549f48e052c3db7c7a885f714d461cc70ea7d15e
Ubuntu <ubuntu@ubuntu.fritz.box>
2021-11-04 02:24:33 +0100

    jenkins test2

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello Jenkinsgit v2 <pre>'+JSON.stringify(process.env)+"</pre>");
+  res.send('Hello Jenkinsgit v2 CCCCC<pre>'+JSON.stringify(process.env)+"</pre>");
 });
 
 app.listen(PORT, HOST);
